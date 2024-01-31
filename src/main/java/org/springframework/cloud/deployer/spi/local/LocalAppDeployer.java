408cfc10aaebe3b383a2ccbb21fc3dcbd6554dfc
Corneil du Plessis <corneil.du-plessis@broadcom.com>
2024-01-31 13:38:30 +0200

    Change imports of Java EE packages to Jakarta EE. (#432)

--- src/main/java/org/springframework/cloud/deployer/spi/local/LocalAppDeployer.java
+++ src/main/java/org/springframework/cloud/deployer/spi/local/LocalAppDeployer.java
@@ -35,7 +35,7 @@ import java.util.Optional;
 import java.util.TreeMap;
 import java.util.concurrent.ConcurrentHashMap;
 
-import javax.annotation.PreDestroy;
+import jakarta.annotation.PreDestroy;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
